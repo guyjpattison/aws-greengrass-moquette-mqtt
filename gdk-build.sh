a08dab4ccef32ed0781f4fd058f4fdeff65cabcc
Guy Pattison <guy.pattison@mistral-data.com>
2023-05-16 14:16:40 +0100

    Recipes attempted correction

--- gdk-build.sh
+++ gdk-build.sh
@@ -9,4 +9,4 @@ mvn clean package -DskipTests
 
 cp integration/target/MistralData.greengrass.clientdevices.mqtt.Moquette.jar \
     greengrass-build/artifacts/MistralData.greengrass.clientdevices.mqtt.Moquette/${VERSION}/ \
-    && cp recipe.json greengrass-build/recipes/
+    && cp recipe.json recipes/
