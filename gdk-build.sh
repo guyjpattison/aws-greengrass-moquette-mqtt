b73dffb2c05fb31abde20b9cf02fe6a0dfb0e054
Guy Pattison <guy.pattison@mistral-data.com>
2023-05-16 13:29:35 +0100

    Update gdk-build to ref name in config

--- gdk-build.sh
+++ gdk-build.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Extract from gdk-config.json
-VERSION=`jq -r '.component."aws.greengrass.clientdevices.mqtt.Moquette".version' gdk-config.json`
+VERSION=`jq -r '.component."MistralData.greengrass.clientdevices.mqtt.Moquette".version' gdk-config.json`
 
 mvn clean package -DskipTests
 
-cp integration/target/aws.greengrass.clientdevices.mqtt.Moquette.jar \
-    greengrass-build/artifacts/aws.greengrass.clientdevices.mqtt.Moquette/${VERSION}/ \
+cp integration/target/MistralData.greengrass.clientdevices.mqtt.Moquette.jar \
+    greengrass-build/artifacts/MistralData.greengrass.clientdevices.mqtt.Moquette/${VERSION}/ \
     && cp recipe.json greengrass-build/recipes/
